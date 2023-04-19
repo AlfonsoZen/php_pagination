30467672e4ae41d04c3ea53f40a3a560f79b9f25
Malafama20 <omarzenlab@gmail.com>
2023-04-18 19:32:56 -0600

    Creating the Pagination Logic

--- index.php
+++ index.php
@@ -7,9 +7,25 @@ try {
   die();
 }
 
-$page = isset( $_GET['page'] ) ? (int)$_GET['pagina'] : 1;
+$page = isset( $_GET['page'] ) ? (int)$_GET['page'] : 1;
 
-$postsPerPage = 5
+$postsPerPage = 5;
+
+$start = ($page > 1) ? ($page * $postsPerPage - $postsPerPage) : 0;
+
+$articles = $connection -> prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articles LIMIT $start, $postsPerPage");
+
+$articles -> execute();
+$articles = $articles -> fetchAll();
+
+if ( !$articles ) {
+  header( 'Location: index.php');
+}
+
+$totalArticles = $connection -> query('SELECT FOUND_ROWS() as total');
+$totalArticles = $totalArticles -> fetch()['total'];
+
+$pagesNumber = ceil( $totalArticles / $postsPerPage );
 require 'index.view.php';
 
 ?>
