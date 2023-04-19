64ca642987efca43ed73cbff75c35ea4c9e5286e
Malafama20 <omarzenlab@gmail.com>
2023-04-18 18:09:48 -0600

    Part 1 - Creating the Pagination Logic

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+
+try {
+  $connection  = new PDO('mysql:host=localhost;dbname=php_pagination', 'root', '');
+} catch (PDOException $e) {
+  echo 'Error' . $e -> getMessage();
+  die();
+}
+
+$page = isset( $_GET['page'] ) ? (int)$_GET['pagina'] : 1;
+
+$postsPerPage = 5
 require 'index.view.php';
 
 ?>
